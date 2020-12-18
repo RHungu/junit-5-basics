d277ce595812d2b0df66693c3c9ba8c5ea3d830f
Raphael Hungu <raphael.hungu@gmail.com>
2020-12-18 18:15:26 +0300

    Various updates to the unit tests

--- src/test/java/com/logicharmony/MathUtilsTest.java
+++ src/test/java/com/logicharmony/MathUtilsTest.java
@@ -2,13 +2,22 @@ package com.logicharmony;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
+	MathUtils mathUtils;
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
 
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
+//		MathUtils mathUtils = new MathUtils();
 		int actual =  mathUtils.add(5, 2);
 		int expected = 7;
 		assertEquals(expected, actual,"Should add two numbers expected and actual");
@@ -16,15 +25,20 @@ class MathUtilsTest {
 	
 	@Test
 	void testComputeCircleArea() {
-		MathUtils mathUtils = new MathUtils();
+//		MathUtils mathUtils = new MathUtils();
 		assertEquals( 314.1592653589793238, mathUtils.computeCircleArea(10),"Should return correct value of circle area");
 	}
 	
 	
 	@Test
 	void testDivide() {
-		MathUtils mathUtils = new MathUtils();
+//		MathUtils mathUtils = new MathUtils();
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),"Divide by zero should throw" );
+	}
+	
+	@AfterAll
+	static void giveOutpu() {
+		System.out.println("We have run all the tests that were required");
 	}
 
 }
